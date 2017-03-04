45bab4f961d272bfda3ca50559ce52c0238ba6d0
BrendaDilschneider <brenda_dilschneider@hotmail.com>
2017-03-04 10:21:24 -0300

    click en item de serie y se abre la serie - me falta completar los datos de serie

---
+++ app/src/main/java/com/example/mel/proyiaw_series_mely/verItemSerieActivity.java
@@ -0,0 +1,13 @@
+package com.example.mel.proyiaw_series_mely;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class verItemSerieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_item_serie);
+    }
+}
